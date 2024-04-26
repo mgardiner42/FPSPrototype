eb249a8894dfcbb11801518871875950f2282d07
mgardiner42 <mgardiner42@yahoo.com>
2024-04-26 13:48:24 -0400

    Launcher Code V.1
    Launcher menu connects players to the Green Map. Still Working on adding Player name functionality. Tweaked Molotov particle effects.

--- Assets/Launcher.cs
+++ Assets/Launcher.cs
@@ -1,86 +1,29 @@
 using UnityEngine;
 using Photon.Pun;
 using Photon.Realtime;
+using UnityEngine.SceneManagement;
 
 namespace Com.MyCompany.MyGame
 {
     public class Launcher : MonoBehaviourPunCallbacks
     {
-        #region Private Serializable Fields
 
         [SerializeField]
         private byte maxPlayersPerRoom = 2;
 
-        #endregion
-
-        #region Private Fields
-
-        /// <summary>
-        /// This client's version number. Users are separated from each other by gameVersion (which allows you to make breaking changes).
-        /// </summary>
+       // This client's version number. Users are separated from each other by gameVersion
         string gameVersion = "1";
 
-        #endregion
-
-        #region MonoBehaviour CallBacks
-
-        /// <summary>
-        /// MonoBehaviour method called on GameObject by Unity during early initialization phase.
-        /// </summary>
         void Awake()
         {
-            // #Critical
             // this makes sure we can use PhotonNetwork.LoadLevel() on the master client and all clients in the same room sync their level automatically
             PhotonNetwork.AutomaticallySyncScene = true;
         }
 
-        /// <summary>
-        /// MonoBehaviour method called on GameObject by Unity during initialization phase.
-        /// </summary>
-        void Start()
+
+        public void Play()
         {
-            //Connect();
-        }
-
-        #endregion
-
-
-        #region Public Methods
-
-        /// <summary>
-        /// Start the connection process.
-        /// - If already connected, we attempt joining a random room
-        /// - if not yet connected, Connect this application instance to Photon Cloud Network
-        /// </summary>
-        public void Connect()
-        {
-            // we check if we are connected or not, we join if we are , else we initiate the connection to the server.
-            if (PhotonNetwork.IsConnected)
-            {
-                // #Critical we need at this point to attempt joining a Random Room. If it fails, we'll get notified in OnJoinRandomFailed() and we'll create one.
-                PhotonNetwork.JoinRandomRoom();
-            }
-            else
-            {
-                // #Critical, we must first and foremost connect to Photon Online Server.
-                PhotonNetwork.ConnectUsingSettings();
-                PhotonNetwork.GameVersion = gameVersion;
-            }
-        }
-
-        #endregion
-
-        public override void OnConnectedToMaster()
-        {
-            PhotonNetwork.JoinRandomRoom();
-        }
-
-        public override void OnJoinRandomFailed(short returnCode, string message)
-        {
-            Debug.Log("PUN Basics Tutorial/Launcher:OnJoinRandomFailed() was called by PUN. No random room available, so we create one.\nCalling: PhotonNetwork.CreateRoom");
-
-            // #Critical: we failed to join a random room, maybe none exists or they are all full. No worries, we create a new room.
-            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = maxPlayersPerRoom });
+            SceneManager.LoadScene("GreenMap");
         }
 
     }
