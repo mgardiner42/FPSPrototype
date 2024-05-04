878a266e43d4346f74b18dead47845227f18a322
Jon2U <mail4jonnyy@gmail.com>
2024-05-04 17:30:09 -0400

    Flag shing sound effect

--- Assets/Flag.cs
+++ Assets/Flag.cs
@@ -7,14 +7,22 @@ using UnityEngine;
 public class Flag : MonoBehaviour
 {
     Renderer[] childRenderers;
+    private AudioSource audioSource;
+    public  AudioClip shing;
+
+
     private void Start(){
         // access mesh renders of each prefab
         childRenderers = GetComponentsInChildren<Renderer>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     [PunRPC]
     private void OnTriggerEnter(Collider collision){
         if (collision.transform.tag == "Player" && transform.parent == null){
+            audioSource.clip = shing;
+            audioSource.Play();
+
             transform.SetParent(collision.transform);
             foreach(Renderer childRenderer in childRenderers){
                 // disable renderers when the flag is grabbed
